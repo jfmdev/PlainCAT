c2da301ceb348b490262a95b2d5764c6898d6e97
Jose <jfmdev@gmail.com>
2018-02-06 20:07:16 -0300

    Move code from Body controller to Main controller

--- code/public/js/controllers/body.js
+++ code/public/js/controllers/body.js
@@ -1,83 +1,38 @@
 // Define controller.
 myApp.controller('bodyController', [
-    '$scope', 'Shared', 'Menu', 'Editor', 'Translator', 'Spellchecker', 'toastr', 
-    function ($scope, Shared, Menu, Editor, Translator, Spellchecker, toastr) {
+    '$scope', 'Shared', 'Menu', 'Editor', 'toastr', 
+    function ($scope, Shared, Menu, Editor, toastr) {
         // Initialize variables.
         $scope.files = Shared.files;
-        var ipcRenderer = require('electron').ipcRenderer;
 
         // ----- Files ----- //
 
-        // Load a file on either the source or the target pane.
-        $scope.loadFile = function(type, fileData) {
-            $scope.files[type].dirty = false;
-            $scope.files[type].name = fileData.name;
-            $scope.files[type].path = fileData.path;
-            $scope.files[type].content = fileData.lines;
-
-            Editor.initialize(
-                type,
-                type === 'source'? '#source-file' : '#target-file',
-                type === 'source'? '#target-file' : '#source-file',
-                fileData.lines
-            );
-        }
+        // Update editors when the content is updated.
+        $scope.$watch('files.source', function() {
+            Editor.initialize('source', '#source-file', '#target-file', $scope.files.source.content || []);
+        });
+        $scope.$watch('files.target', function() {
+            Editor.initialize('target', '#target-file', '#source-file', $scope.files.target.content || []);
+        });
 
         // Copy the source's content into the target.
         $scope.copySource = function() {
             var lines = Editor.getContent('#source-file');
-            var fileData = { lines: [] };
+            var content = [];
             for(var i=0; i<lines.length; i++) {
-                fileData.lines.push({
-                    'index': (i>0? (fileData.lines[i-1].index + lines[i-1].length) : 0),
+                content.push({
+                    'index': (i>0? (content[i-1].index + lines[i-1].length) : 0),
                     'text': lines[i]
                 })
             }
-            $scope.loadFile('target', fileData);
+
+            // Update target (this will trigger the editor's initialization via the watcher)
+            $scope.files.target = {
+                dirty: true,
+                name: null,
+                path: null,
+                content: content,
+            };
         };
-
-        // Event handler for when a file is read.
-        $scope.onFileRead = function(event, result) {   
-            // Verify if the operation was successful.
-            if(result) {
-                if(!result.error) {
-                    // Load file.
-                    $scope.loadFile(result.target, result.data);
-                } else {
-                    // Display warning or error.
-                    if(result.error.code === 'NO_FILE_SELECTED') {
-                        toastr.info('No file was selected');
-                    } else {
-                        toastr.error(result.error.message || "File couldn't be read", 'Error');
-                    }
-                }
-            }
-
-            // Update UI.
-            $scope.$apply();
-        }
-
-        // Function for open a file selector.
-        $scope.openFile = function(target) {
-            // Open file selector dialog.
-            ipcRenderer.send('open-file', target); 
-        };
-
-        // Initialize menu, defining methods for open files.
-        Menu.init({
-            openSource: function(item, focusedWindow) { 
-                $scope.openFile('source'); 
-                $scope.$apply(); 
-            },
-            openTarget: function(item, focusedWindow) { 
-                $scope.openFile('target'); 
-                $scope.$apply(); 
-            },
-        });
-        ipcRenderer.on('file-read', $scope.onFileRead);
-
-        // Load (if availables) the last opened files.
-        ipcRenderer.send('last-file', 'source');
-        ipcRenderer.send('last-file', 'target');
     }
 ]);
--- code/public/js/controllers/main.js
+++ code/public/js/controllers/main.js
@@ -1,7 +1,63 @@
 // Define controller.
 myApp.controller('mainController', [
-    '$scope', 'Menu', 'Editor', 'Translator', 'Spellchecker', 'toastr', 
-    function ($scope, Menu, Editor, Translator, Spellchecker, toastr) {
+    '$scope', 'Shared', 'Menu', 'toastr', 
+    function ($scope, Shared, Menu, toastr) {
+        var ipcRenderer = require('electron').ipcRenderer;
 
+        // ----- Files ----- //
+
+        // Load a file on either the source or the target pane.
+        $scope.loadFile = function(type, fileData) {
+            Shared.files[type] = {
+                dirty: false,
+                name: fileData.name,
+                path: fileData.path,
+                content: fileData.lines,
+            };
+        }
+
+        // Event handler for when a file is read.
+        $scope.onFileRead = function(event, result) {   
+            // Verify if the operation was successful.
+            if(result) {
+                if(!result.error) {
+                    // Load file.
+                    $scope.loadFile(result.target, result.data);
+                } else {
+                    // Display warning or error.
+                    if(result.error.code === 'NO_FILE_SELECTED') {
+                        toastr.info('No file was selected');
+                    } else {
+                        toastr.error(result.error.message || "File couldn't be read", 'Error');
+                    }
+                }
+            }
+
+            // Update UI.
+            $scope.$apply();
+        }
+
+        // Function for open a file selector.
+        $scope.openFile = function(target) {
+            // Open file selector dialog.
+            ipcRenderer.send('open-file', target); 
+        };
+
+        // Initialize menu, defining methods for open files.
+        Menu.init({
+            openSource: function(item, focusedWindow) { 
+                $scope.openFile('source'); 
+                $scope.$apply(); 
+            },
+            openTarget: function(item, focusedWindow) { 
+                $scope.openFile('target'); 
+                $scope.$apply(); 
+            },
+        });
+        ipcRenderer.on('file-read', $scope.onFileRead);
+
+        // Load (if availables) the last opened files.
+        ipcRenderer.send('last-file', 'source');
+        ipcRenderer.send('last-file', 'target');
     }
 ]);
