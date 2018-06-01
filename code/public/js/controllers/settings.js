f7097953ae8e030acc606fe8e08f5dbff60c3bcb
Jose <jfmdev@gmail.com>
2018-05-31 23:20:01 -0300

    Start implementation of General tab from Settings dialog

--- code/public/js/controllers/settings.js
+++ code/public/js/controllers/settings.js
@@ -1,18 +1,22 @@
 // Define controller.
 myApp.controller('settingsController', [
-    '$scope', '$rootScope', 'Shared', 'Translator', '$uibModalInstance', 'tab',
-    function ($scope, $rootScope, Shared, Translator, $uibModalInstance, tab) {
+    '$scope', '$rootScope', 'Shared', 'Languages', 'Translator', '$uibModalInstance', 'tab',
+    function ($scope, $rootScope, Shared, Languages, Translator, $uibModalInstance, tab) {
+        // Initialize active tab.
         $scope.activeTab = 0;
         if(tab === 'general') { $scope.activeTab = 0; }
         if(tab === 'translation') { $scope.activeTab = 1; }
         if(tab === 'about') { $scope.activeTab = 2; }
 
-        $scope.cancel = function () {
-            $uibModalInstance.dismiss('cancel');
-        };
+        // Get list of languages.
+        $scope.languages = Languages.list;
 
+        // Save and cancel buttons.
         $scope.save = function () {
             alert('Feature not implemented');
+        };
+        $scope.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
         };
     }
 ]);
