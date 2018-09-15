738d5e95431c31098cd772f6600674f77525d9ad
Jose <jfmdev@gmail.com>
2018-09-15 11:57:27 -0300

    Add icon

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({ width: 800, height: 600, icon: 'img/icon-32.png' });
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/public/index.html');
